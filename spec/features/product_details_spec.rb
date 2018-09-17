96f5c6161ff3b6b4c9798b1fa62fac3f881a557c
Geoff Phillips <geoffreymartinphillips@gmail.com>
2018-09-17 16:53:03 -0700

    Implement feature spec for product#show

---
+++ spec/features/product_details_spec.rb
@@ -0,0 +1,28 @@
+require 'rails_helper'
+
+RSpec.feature "Visitor navigates to product page", type: :feature, js: true do
+
+  before :each do
+    @category = Category.create! name: 'Apparel'
+
+    10.times do |n|
+      @category.products.create!(
+        name:  Faker::Hipster.sentence(3),
+        description: Faker::Hipster.paragraph(4),
+        image: open_asset('apparel1.jpg'),
+        quantity: 10,
+        price: 64.99
+      )
+    end
+  end
+
+
+  scenario "They see product image" do
+    visit product_path(1)
+
+    # save_screenshot
+
+    expect(page).to have_css 'img.main-img', count: 1
+  end
+
+end
